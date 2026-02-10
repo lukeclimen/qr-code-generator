9f07881273d7bdc73bab44557bf97444613bdb9d
Luke Climenhage <luke.climenhage@gmail.com>
2026-02-09 21:45:57 -0800

    Grab defaults from URL if present

--- src/app/ui/QrCodeForm.tsx
+++ src/app/ui/QrCodeForm.tsx
@@ -2,10 +2,22 @@
 
 import Form from "next/form";
 import { useState } from "react";
+import { useSearchParams } from "next/navigation";
+
+import ColourInput from "./ColourInput";
 
 export default function QrCodeForm() {
-  const [pixelColour, setPixelColour] = useState("#000000");
-  const [backgroundColour, setBackgroundColour] = useState("#000000");
+  const searchParams = useSearchParams();
+  // Form defaults from query params
+  const defaultPixelColour = searchParams.get("pixelColour");
+  const defaultBackgroundColour = searchParams.get("backgroundColour");
+
+  const [pixelColour, setPixelColour] = useState(
+    defaultPixelColour || "#000000",
+  );
+  const [backgroundColour, setBackgroundColour] = useState(
+    defaultBackgroundColour || "#000000",
+  );
 
   return (
     <Form
@@ -24,7 +36,6 @@ export default function QrCodeForm() {
           required
         />
       </div>
-
       <div className="flex flex-col gap-1">
         <label htmlFor="fileName" className="text-sm font-medium text-gray-700">
           File Name
@@ -37,63 +48,18 @@ export default function QrCodeForm() {
           required
         />
       </div>
-
-      <div className="flex flex-col gap-1">
-        <label
-          htmlFor="pixelColour"
-          className="text-sm font-medium text-gray-700"
-        >
-          Pixel Color
-        </label>
-        <div className="flex flex-row gap-x-5">
-          <input
-            className="h-12.5 border border-black aspect-square rounded-md focus:outline-none"
-            name="pixelColour"
-            id="pixelColour"
-            type="color"
-            value={pixelColour}
-            onChange={(e) => setPixelColour(e.target.value)}
-          />
-          <input
-            className="p-3 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            type="text"
-            value={pixelColour}
-            onChange={(e) => setPixelColour(e.target.value)}
-            pattern="^#[0-9A-Fa-f]{6}$"
-            maxLength={7}
-            placeholder="#000000"
-          />
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-1">
-        <label
-          htmlFor="backgroundColour"
-          className="text-sm font-medium text-gray-700"
-        >
-          Background Color
-        </label>
-        <div className="flex flex-row gap-x-5">
-          <input
-            className="h-12.5 border border-black aspect-square rounded-md focus:outline-none"
-            name="backgroundColour"
-            id="backgroundColour"
-            type="color"
-            value={backgroundColour}
-            onChange={(e) => setBackgroundColour(e.target.value)}
-          />
-          <input
-            className="p-3 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            type="text"
-            value={backgroundColour}
-            onChange={(e) => setBackgroundColour(e.target.value)}
-            pattern="^#[0-9A-Fa-f]{6}$"
-            maxLength={7}
-            placeholder="#000000"
-          />
-        </div>
-      </div>
-
+      <ColourInput
+        label="Pixel Color"
+        id="pixelColour"
+        value={pixelColour}
+        onChange={setPixelColour}
+      />
+      <ColourInput
+        label="Background Color"
+        id="backgroundColour"
+        value={backgroundColour}
+        onChange={setBackgroundColour}
+      />
       <button
         type="submit"
         className="mt-4 p-3 bg-linear-to-br from-blue-500 to-purple-600 text-white cursor-pointer rounded-md font-medium transition-colors"
