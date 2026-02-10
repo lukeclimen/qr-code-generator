94143ff7a08572b6518acfeb952ce97046e8bfa5
Luke Climenhage <luke.climenhage@gmail.com>
2026-02-09 21:29:26 -0800

    Add form to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
+import QrCodeForm from "./ui/QrCodeForm";
+
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gradient-to-br from-blue-500 to-purple-600">
-      <div className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="min-h-screen bg-linear-to-br from-blue-500 to-purple-600">
+      <div className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-5">
         <h1 className="text-5xl font-bold text-white drop-shadow-lg">
-          Hello World
+          QR Code Generator
         </h1>
-        <p className="mt-4 text-xl text-white/80">
-          Welcome to your QR Code Generator
-        </p>
+        <QrCodeForm />
       </div>
     </main>
   );
