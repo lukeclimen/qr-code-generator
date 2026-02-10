b0320c32c1f2cdc3a8fea5790e1b24ab030516f9
Luke Climenhage <luke.climenhage@gmail.com>
2026-02-09 20:28:46 -0800

    Add tailwind to project

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/qr-code-generator/", // <=== replace with your repo name (must start with a /)
-  assetPrefix: "/qr-code-generator/", // <=== replace with your repo name (must end with a /)
   images: {
     unoptimized: true, // Disables Next.js image optimization which requires a server
   },
+  basePath: "/qr-code-generator",
+  assetPrefix: "/qr-code-generator",
   output: "export",
   images: {
     unoptimized: true,
